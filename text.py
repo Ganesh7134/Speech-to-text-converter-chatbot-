466223725ce0f4b632afc464d07853a04978fe6b
Ganesh7134 <136222889+Ganesh7134@users.noreply.github.com>
2024-04-15 11:50:49 +0530

    Update text.py

--- text.py
+++ text.py
@@ -3,6 +3,7 @@ import speech_recognition
 from time import sleep
 import json
 from streamlit_lottie import st_lottie
+from deep_translator import GoogleTranslator
 
 container = st.container()
 try:
@@ -20,7 +21,140 @@ except:
 st.title("Speech to text converter chatbot 🤖")
 st.warning("**Note**: the loop will runs infinitely until your speech recognize by the recognizer")
 
-dic = {"Hindi":"hi-IN" , "English-india":"en-in" , "English-USA":"en-US" , "English-United Kingdom":"en-GB","tamil":"ta-IN"}
+dic = {"Afrikaans":"af",
+"Albanian":"sq",
+"Amharic":"am",
+"Arabic":"ar",
+"Armenian":"hy",
+"Assamese":"as",
+"Aymara":"ay",
+"Azerbaijani":"az",
+"Bambara":"bm",
+"Basque":"eu",
+"Belarusian":"be",
+"Bengali":"bn",
+"Bhojpuri":"bho",
+"Bosnian":"bs",
+"Bulgarian":"bg",
+"Catalan":"ca",
+"Cebuano":"ceb",
+"Chinese":"zh-CN", 
+"Chinese":"zh-TW", 
+"Corsican":"co",
+"Croatian":"hr",
+"Czech":"cs",
+"Danish":"da",
+"Dhivehi":"dv",
+"Dogri":"doi",
+"Dutch":"nl",
+"English":"en",
+"Esperanto":"eo",
+"Estonian":"et",
+"Ewe":"ee",
+"Filipino":"fil",
+"Finnish":"fi",
+"French":"fr",
+"Frisian":"fy",
+"Galician":"gl",
+"Georgian":"ka",
+"German":"de",
+"Greek":"el",
+"Guarani":"gn",
+"Gujarati":"gu",
+"Haitian Creole":"ht",
+"Hausa":"ha",
+"Hawaiian":"haw",
+"Hebrew":"he",
+"Hindi":"hi",
+"Hmong":"hmn",
+"Hungarian":"hu",
+"Icelandic":"is",
+"Igbo":"ig",
+"Ilocano":"ilo",
+"Indonesian":"id",
+"Irish":"ga",
+"Italian":"it",
+"Japanese":"ja",
+"Javanese":"jv",
+"Kannada":"kn",
+"Kazakh":"kk",
+"Khmer":"km",
+"Kinyarwanda":"rw",
+"Konkani":"gom",
+"Korean":"ko",
+"Krio":"kri",
+"Kurdish":"ku",
+"Kurdish(Sorani)":"ckb",
+"Kyrgyz":"ky",
+"Lao":"lo",
+"Latin":"la",
+"Latvian":"lv",
+"Lingala":"ln",
+"Lithuanian":"lt",
+"Luganda":"lg",
+"Luxembourgish":"lb",
+"Macedonian":"mk",
+"Maithili":"mai",
+"Malagasy":"mg",
+"Malay":"ms",
+"Malayalam":"ml",
+"Maltese":"mt",
+"Maori":"mi",
+"Marathi":"mr",
+"Meiteilon(Manipuri)":"mni-Mtei",
+"Mizo":"lus",
+"Mongolian":"mn",
+"Myanmar(Burmese)":"my",
+"Nepali":"ne",
+"Norwegian":"no",
+"Nyanja(Chichewa)":"ny",
+"Odia(Oriya)":"or",
+"Oromo":"om",
+"Pashto":"ps",
+"Persian":"fa",
+"Polish":"pl",
+"Portuguese(Portugal, Brazil)":"pt",
+"Punjabi":"pa",
+"Quechua":"qu",
+"Romanian":"ro",
+"Russian":"ru",
+"Samoan":"sm",
+"Sanskrit":"sa",
+"Scots Gaelic":"gd",
+"Sepedi":"nso",
+"Serbian":"sr",
+"Sesotho":"st",
+"Shona":"sn",
+"Sindhi":"sd",
+"Sinhala (Sinhalese)":"si",
+"Slovak":"sk",
+"Slovenian":"sl",
+"Somali":"so",
+"Spanish":"es",
+"Sundanese":"su",
+"Swahili":"sw",
+"Swedish":"sv",
+"Tagalog (Filipino)":"tl",
+"Tajik":"tg",
+"Tamil":"ta",
+"Tatar":"tt",
+"Telugu":"te",
+"Thai":"th",
+"Tigrinya":"ti",
+"Tsonga":"ts",
+"Turkish":"tr",
+"Turkmen":"tk",
+"Twi (Akan)":"ak",
+"Ukrainian":"uk",
+"Urdu":"ur",
+"Uyghur":"ug",
+"Uzbek":"uz",
+"Vietnamese":"vi",
+"Welsh":"cy",
+"Xhosa":"xh",
+"Yiddish":"yi",
+"Yoruba":"yo",
+"Zulu":"zu"}
 lang = st.selectbox("select language to transribe english to your selected language: ",list(dic.keys()))
 
 def takeCommand():
@@ -47,10 +181,14 @@ def takeCommand():
             sleep(0.5)
             with st.chat_message("assistant"):
                 st.markdown("understanding...")
-            query = r.recognize_google(audio,language = dic[lang])
+            query = r.recognize_google(audio,language = "en-IN")
+            translation = GoogleTranslator(source='en', target=dic[lang]).translate(query)
+            
             sleep(0.5)
             with st.chat_message("assistant"):
-                st.markdown(f"you said : {query}")
+                st.header(f"you said : {query}")
+            with st.chat_message("assistant"):
+                st.subheader(f"Translated text: {translation}")
         except Exception as e:
             print("Say that again")
             return takeCommand() # if any mistake happens then recursive function will call again...
